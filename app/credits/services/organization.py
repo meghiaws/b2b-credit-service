5738343849106ca91c53d502bf135224402dcd3e
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2023-06-20 03:28:06 +0330

    chore: add logging to transfer and increase-balance

--- app/credits/services/organization.py
+++ app/credits/services/organization.py
@@ -1,10 +1,14 @@
+import logging
 from decimal import Decimal
+
 from django.db import transaction
 from django.db.models import QuerySet, F
 from django.contrib.auth import get_user_model
 
 from app.credits.models import IncreaseBalanceTransaction, Organization
 from app.credits.exceptions import OneTimeIncreasedBalanceException
+
+logger = logging.getLogger(__name__)
 
 User = get_user_model()
 
@@ -34,23 +38,30 @@ class OrganizationService:
             .get()
         )
 
+        # check whether the organization has ever charged its account or not
         already_increased_balance = IncreaseBalanceTransaction.objects.filter(
             receiver_id=organization_id
         ).exists()
-
-        # check whether the organization has ever charged its account or not
         if already_increased_balance:
+            logger.exception(
+                f"The organization {organization_id} has already used its one time chance to charge its account"
+            )
             raise OneTimeIncreasedBalanceException(
                 {"message": "you have used your one-time chance to charge your account"}
             )
 
+        logger.info("Starting increase balance process.")
         with transaction.atomic():
             # create increase balance record for keep track of increased balances
+            logger.info("Creating increase balance transaction.")
             IncreaseBalanceTransaction.objects.create(
                 receiver_id=organization_id, amount=balance
             )
 
             # update the actual balance of the organization
+            logger.info(f"Updating balance of organization {organization_id}.")
             Organization.objects.filter(id=organization_id).update(
                 balance=F("balance") + balance
             )
+
+        logger.info(f"Increase balance of organization {organization_id} finished.")
--- app/credits/services/transfer.py
+++ app/credits/services/transfer.py
@@ -1,4 +1,6 @@
+import logging
 from decimal import Decimal
+
 from django.db import transaction
 from django.contrib.auth import get_user_model
 
@@ -9,6 +11,7 @@ from app.credits.exceptions import (
     TransferDestinationNotValid,
 )
 
+logger = logging.getLogger(__name__)
 
 User = get_user_model()
 
@@ -23,10 +26,14 @@ class TransferService:
             phone=destination_phone
         ).exists()
         if not destination_exists:
+            logger.exception(
+                f"There is no organization with {destination_phone} phone number."
+            )
             raise TransferDestinationNotFound(
                 {"message": "there is no organization with provided phone number"}
             )
 
+        logger.info("Starting the credit transfer process.")
         with transaction.atomic():
             receiver_organization = Organization.objects.select_for_update().get(
                 phone=destination_phone
@@ -38,17 +45,24 @@ class TransferService:
 
             # check whether sender organization not trying to transfer credit to itself
             if sender_organization.id == receiver_organization.id:
+                logger.exception(
+                    f"Cannot transfer money from {sender_organization.id} to {receiver_organization.id}."
+                )
                 raise TransferDestinationNotValid(
                     {"message": "you cannot transfer money to yourself"}
                 )
 
             # check if sender organization has enough money to transfer
             if sender_organization.balance < amount:
+                logger.exception(
+                    f"Sender balance ({sender_organization.balance}) is lower than transfer amount ({amount})."
+                )
                 raise NotEnoughCreditsException(
                     {"message": "the sender balance is lower than amount"}
                 )
 
             # create transfer balance record for keep track of increased balances
+            logger.info("Creating record for transfer transactions.")
             TransferTransaction.objects.create(
                 sender_id=sender_organization.id,
                 receiver_id=receiver_organization.id,
@@ -56,7 +70,17 @@ class TransferService:
             )
 
             # withdraw the actual balance from the sender organization
+            logger.info(
+                f"Withdrawing credits from organization {sender_organization.name}."
+            )
             sender_organization.withdraw(amount=amount)
 
             # deposit the actual balance of the organization
+            logger.info(
+                f"Depositing credits to organization {receiver_organization.name}."
+            )
             receiver_organization.deposit(amount=amount)
+
+        logger.info(
+            f"Transferring credits from {sender_organization.name} to organization {receiver_organization.name} finished."
+        )
