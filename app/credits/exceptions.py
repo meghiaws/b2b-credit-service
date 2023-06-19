543fac7cd78061ccce30e3714f5ffc52a40686d4
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2023-06-20 03:25:49 +0330

    chore: add more custom exception types

--- app/credits/exceptions.py
+++ app/credits/exceptions.py
@@ -14,3 +14,15 @@ class NotEnoughCreditsException(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = _("The sender balance is lower than amount.")
     default_code = "not_enough_credits_exception"
+
+
+class TransferDestinationNotValid(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _("You cannot transfer money to yourself.")
+    default_code = "transfer_destination_not_valid"
+
+
+class TransferDestinationNotFound(APIException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = _("There is no organization with provided phone number.")
+    default_code = "transfer_destination_not_found"
