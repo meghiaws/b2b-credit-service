76b9e8edf732b129eed06d00f8cf67ee963de65f
Mahdi Ghiasi <mahdiw.ghiasi@gmail.com>
2023-06-28 03:55:18 +0330

    chore: validate transfer amount properly

--- app/credits/serializers/transfer.py
+++ app/credits/serializers/transfer.py
@@ -3,7 +3,15 @@ from rest_framework import serializers, status
 
 
 class TransferInputSerializer(serializers.Serializer):
-    destination_phone = serializers.CharField()
+    organization_id = serializers.IntegerField(min_value=1)
+    customer_phone = serializers.CharField()
     amount = serializers.DecimalField(
         max_digits=12, decimal_places=2, min_value=Decimal(0.01)
     )
+
+    def validate_amount(self, value: Decimal):
+        if float(value) < 0.01:
+            raise serializers.ValidationError(
+                {"message": "amount must be greater than or equal to 0.01"}
+            )
+        return value
